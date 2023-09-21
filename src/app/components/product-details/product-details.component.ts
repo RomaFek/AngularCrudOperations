84660978d8a458b4c44aad1d88d1570d66b339f7
Roman Feoktistov <rqvyzz@gmail.com>
2023-09-21 16:38:41 +0300

    fix unsubscribe

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { IProducts } from 'src/app/models/products';
@@ -8,15 +8,21 @@ import { IProducts } from 'src/app/models/products';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
   product: IProducts
   productsSubscription: Subscription
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.productsSubscription = this.route.data.subscribe( (data)=> {
-      this.product = data['data']
-    })
+    this.productsSubscription = this.route.data.subscribe((data) => {
+      this.product = data['data'];
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
+  }
 }
